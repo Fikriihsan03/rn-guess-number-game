9e6932f57c5c57ab9285b8df050c4b51f7d63e87
Muhammad Fikri Ihsan <mfikriihsan03@gmail.com>
2022-12-23 12:04:44 +0700

    style: styling gameover screen

--- screens/GameOverScreen.tsx
+++ screens/GameOverScreen.tsx
@@ -1,7 +1,41 @@
-import { Text } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
+import { Colors } from "../components/constants/Colors";
+import Title from "../components/ui/Title";
 
 const GameOverScreen = () => {
-  return <Text>Our Game Is Over</Text>;
+  return (
+    <View style={styles.rootContainer}>
+      <Title>Game Over</Title>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require("../assets/images/success.png")}
+        />
+      </View>
+    </View>
+  );
 };
 
 export default GameOverScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    padding: 24,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 2,
+    borderColor: Colors.primary700,
+    overflow: "hidden",
+    margin: 36,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
