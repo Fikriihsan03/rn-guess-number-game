161d31ed073a8723e06bce898d8238b3a83649d7
Muhammad Fikri Ihsan <mfikriihsan03@gmail.com>
2022-12-23 09:45:48 +0700

    feat: create greater and lower button

--- screens/GameScreen.tsx
+++ screens/GameScreen.tsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Alert, StyleSheet, Text, View } from "react-native";
 import NumberContainer from "../components/game/NumberContainer";
+import PrimaryButton from "../components/ui/PrimaryButton";
 import Title from "../components/ui/Title";
 
 interface IProps {
   numberToGuess: number;
 }
+let maxBoundary: number = 100;
+let minBoundary: number = 1;
+
 function generateRandomNumber(
   min: number,
   max: number,
@@ -16,12 +20,44 @@ function generateRandomNumber(
   return randomNumber;
 }
 const GameScreen = ({ numberToGuess }: IProps) => {
-  const initialGuess = generateRandomNumber(1, 100, numberToGuess);
+  const initialGuess = generateRandomNumber(
+    minBoundary,
+    maxBoundary,
+    numberToGuess
+  );
   const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+  const nextGuessHandler = (direction: string) => {
+    if (
+      (direction == "lower" && currentGuess < numberToGuess) ||
+      (direction == "higher" && currentGuess > numberToGuess)
+    ) {
+      Alert.alert("Dont't lie", "You know your number right ?", [
+        { text: "Sorry", style: "cancel" },
+      ]);
+      return;
+    }
+    if (direction === "lower") maxBoundary = currentGuess;
+    if (direction === "higher") minBoundary = currentGuess + 1;
+    const nextGuessNumber = generateRandomNumber(
+      minBoundary,
+      maxBoundary,
+      currentGuess
+    );
+    setCurrentGuess(nextGuessNumber);
+  };
   return (
     <View style={styles.screen}>
       <Title>Opponent's Guess</Title>
       <NumberContainer>{currentGuess}</NumberContainer>
+      <View>
+        <PrimaryButton onPress={nextGuessHandler.bind(this, "higher")}>
+          +
+        </PrimaryButton>
+        <PrimaryButton onPress={nextGuessHandler.bind(this, "lower")}>
+          -
+        </PrimaryButton>
+      </View>
     </View>
   );
 };
